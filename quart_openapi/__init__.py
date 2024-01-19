e849e4f301bacaa027a0a47518096d431f5caf81
kow <kow@xploit.kr>
2024-01-19 22:35:06 +0900

    remove swagger import

--- quart_openapi/__init__.py
+++ quart_openapi/__init__.py
@@ -10,7 +10,7 @@ Documentation can be found on https://factset.github.io/quart-openapi/
 # -*- coding: utf-8 -*-
 from .resource import Resource
 from .pint import Pint, OpenApiView, PintBlueprint
-from .swagger import Swagger
+#from .swagger import Swagger
 from .__about__ import __short_version__, __description__, __release__
 
 # make sure that the module names for all of these are set to
@@ -18,7 +18,7 @@ from .__about__ import __short_version__, __description__, __release__
 Resource.__module__ = __name__
 Pint.__module__ = __name__
 PintBlueprint.__module__ = __name__
-Swagger.__module__ = __name__
+#Swagger.__module__ = __name__
 
 __all__ = [
     '__short_version__',
@@ -27,6 +27,6 @@ __all__ = [
     'Pint',
     'PintBlueprint',
     'Resource',
-    'Swagger',
+    #'Swagger',
     'OpenApiView'
 ]
